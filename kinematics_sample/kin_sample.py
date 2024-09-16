599ca92aec6cdfd8ae27f043ac8b0c591504d5a5
Trang Nguyen <thuy-trang.nguyen@tu-braunschweig.de>
2024-09-17 01:19:37 +0700

    implement ik() function

--- kinematics_sample/kin_sample.py
+++ kinematics_sample/kin_sample.py
@@ -35,3 +35,32 @@ class KinematicsSample(Kinematics):
         result = np.array([x, y, z, 0, 0, 0])
 
         return result
+
+    def ik(self, pose, qInOut=None, name="", optionals=[0, 0], results=[0]):
+        """
+        Compute inverse kinematics of the 3R robot model.
+        """
+        l0, l1, l2, l3 = self.links
+        x3, y3, z3 = pose[:3]
+        alpha = pose[4]
+
+        if l1+l2+l3 < np.sqrt(x3**2 + z3**2):
+            raise ValueError("Target out of reach.")
+
+        # the second joint 
+        x2 = x3 - l3 * np.sin(alpha)
+        z2 = z3 - l3 * np.cos(alpha)
+
+        # Solve for theta2 by using the law of cosines
+        cosTheta2 = (x2**2 + (z2-l0)**2 - l1**2 - l2**2) / (2 * l1 * l2)
+        theta2 = np.arccos(cosTheta2)
+
+        # Solve for theta1
+        phi1 = np.arctan2(x2, z2-l0)
+        phi2 = np.arctan2(l2 * np.sin(theta2), l1 + l2 * np.cos(theta2))
+        theta1 = phi1 - phi2
+
+        # Solve for theta3 
+        theta3 = alpha - (theta1 + theta2)
+        
+        return np.array([theta1, theta2, theta3])
